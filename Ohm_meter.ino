b03b7d2631df020d020be2319a5bd375022c3934
isajak <54275313+isajak@users.noreply.github.com>
2022-04-03 17:18:43 +0200

    Update Ohm_meter.ino

--- Ohm_meter.ino
+++ Ohm_meter.ino
@@ -1,4 +1,7 @@
+// Defines pin
 int analogPin = 0;
+
+// Values for calculations
 int raw = 0;
 int Vin = 5;
 float Vout = 0;
@@ -11,7 +14,11 @@ Serial.begin(9600);
 }
 
 void loop(){
+  
+  // Reads voltage in 10 bits (0-1023)
   raw = analogRead(analogPin);
+  
+  // Calculates the resistance of the unknown resistor
   if(raw){
     buffer = raw * Vin;
     Vout = (buffer)/1024.0;
